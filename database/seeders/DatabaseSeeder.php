14d6075df387f295daaf0ca46b84704fea993d06
David-DC179 <davidchristophersenior@gmail.com>
2026-05-18 11:33:15 +0300

    Remove embedded repository

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Book;
+use App\Models\Review;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            // Create 0-5 reviews for each book
+            Review::factory($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+
+        });
+
+           Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            // Create 0-5 reviews for each book
+            Review::factory($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+
+        });
+
+           Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            // Create 0-5 reviews for each book
+            Review::factory($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+
+        });
+    }
+}
